947e201133b3dea2e4fff8e1dcbe91939da35bca
SlikeX <thelastslike@gmail.com>
2022-03-21 11:29:48 +0500

    feat: change expense controller for db purposes

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,17 +1,31 @@
-const getAllExpenses = (req, res) =>{
-  res.send('Expense')
+const mongoose = require('mongoose');
+const Expense = require('../models/expenseModel');
+
+
+async function getAllExpenses(req, res) {
+  const expense = await Expense.find();
+  res.send(expense);
 };
 
 const getExpById = (req, res) => {
-  res.send(`Expense ID ${req.params.id}`)
+  const expense = await Expense.findById(req.params.id);
+  if (expense.length === 0) {
+    return res.status(404).send('No such expense');
+  }
+  res.status(200).send(expense);
 };
 
 const addExprense = (req, res) =>{
-  res.send('Expense post')
+  const expense = new Expense({
+    expense: req.body.expense,
+  });
+  await expense.save();
+  res.send(expense);
 };
 
 const removeExprense = (req, res) =>{
-  res.send(`delete Expense ${req.params.id}`)
+  await Expense.findOneAndDelete({ id: req.body.id });
+  res.send(`Expense has been deleted`);
 };
 
 module.exports = {
