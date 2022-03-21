d50afb510a547667c6f9ff112c4711deffa980d9
SlikeX <thelastslike@gmail.com>
2022-03-21 11:36:47 +0500

    feat: change income controller for db purposes

--- controllers/imcomeController.js
+++ controllers/imcomeController.js
@@ -1,17 +1,31 @@
-const getAllIncomes = (req, res) =>{
-  res.send('Income')
+const mongoose = require('mongoose');
+const Income = require('../models/incomeModel');
+
+
+async function getAllExpenses(req, res) {
+  const income = await Income.find();
+  res.send(income);
 };
 
-const getIncById = (req, res) =>{
-  res.send(`Income ID ${req.params.id}`)
+const getExpById = (req, res) => {
+  const income = await Income.findById(req.params.id);
+  if (income.length === 0) {
+    return res.status(404).send('No such income');
+  }
+  res.status(200).send(income);
 };
 
-const addIncome = (req, res) =>{
-  res.send('Income post')
+const addExprense = (req, res) =>{
+  const income = new Income({
+    income: req.body.income,
+  });
+  await income.save();
+  res.send(income);
 };
 
-const removeIncome = (req, res) =>{
-  res.send(`Income delete ${req.params.id}`)
+const removeExprense = (req, res) =>{
+  await Income.findOneAndDelete({ id: req.body.id });
+  res.send(`Income has been deleted`);
 };
 
 module.exports = {
